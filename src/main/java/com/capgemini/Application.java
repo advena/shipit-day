d909215a5e2190e9a03e77763b0ce454776687bb
marcinmarkus <marcin.markus@gmail.com>
2016-04-21 23:02:19 +0200

    scheduler

--- src/main/java/com/capgemini/Application.java
+++ src/main/java/com/capgemini/Application.java
@@ -1,11 +1,14 @@
 package com.capgemini;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
+@EnableAutoConfiguration
 @SpringBootApplication
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
